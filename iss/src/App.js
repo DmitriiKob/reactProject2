d790cc9cd48e07c45efea254deec95c871ddc070
James Chin <doaa@Jamess-Air-2.wifi.smc.edu>
2023-04-20 17:34:32 -0700

    learn react

--- iss/src/App.js
+++ iss/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >/
-          Learn React asdf
+          Learn React
         </a>
       </header>
     </div>
